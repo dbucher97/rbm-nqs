69cc54d70debdeb8fbce8f50390d38f80a2489e1
David Bucher <ru93vav@cm2login1.cos.lrz.de>
2021-07-26 17:57:40 +0200

    cleanup

--- cluster/src/tools/ini.cpp.d
+++
@@ -1,3 +0,0 @@
-build/../src/tools/ini.cpp.o: ../src/tools/ini.cpp \
- ../include/tools/ini.hpp
-../include/tools/ini.hpp:
--- cluster/src/tools/logger.cpp.d
+++
@@ -1,4 +0,0 @@
-build/../src/tools/logger.cpp.o: ../src/tools/logger.cpp \
- ../include/tools/ini.hpp ../include/tools/logger.hpp
-../include/tools/ini.hpp:
-../include/tools/logger.hpp:
