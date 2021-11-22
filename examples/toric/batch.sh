b5def5cf1a152eb14c42c64b38d6dc002562dfc6
David Bucher <d.bucher97@gmail.com>
2021-11-22 20:32:37 +0100

    pfaffian inverse fix

--- examples/toric/batch.sh
+++ examples/toric/batch.sh
@@ -14,26 +14,26 @@ function run() {
   fi
 }
 
-echo "===== N=2 Perfect Sampling ====="
-name=toric_n2
-run $name --sampler.type=full
+# echo "===== N=2 Perfect Sampling ====="
+# name=toric_n2
+# run $name --sampler.type=full
 
-echo -e "\n\n===== N=3 Metropolis Sampling ====="
-name=toric_n3
-run $name --model.n_cells=3 --seed=342539432
+# echo -e "\n\n===== N=3 Metropolis Sampling ====="
+# name=toric_n3
+# run $name --model.n_cells=3 --seed=342539432
 
-echo -e "\n\n===== N=3 Metropolis Sampling no plaq ====="
-name=toric_n3_p0
-run $name --model.n_cells=3 --sampler.metropolis.bond_flips=0 --seed=342539434
+# echo -e "\n\n===== N=3 Metropolis Sampling no plaq ====="
+# name=toric_n3_p0
+# run $name --model.n_cells=3 --sampler.metropolis.bond_flips=0 --seed=342539434
 
-echo -e "\n\n===== N=4 Metropolis Sampling ====="
-name=toric_n4
-run $name --model.n_cells=4 --seed=34253943324
+# echo -e "\n\n===== N=4 Metropolis Sampling ====="
+# name=toric_n4
+# run $name --model.n_cells=4 --seed=34253943324
 
 echo -e "\n\n===== N=6 Metropolis Sampling ====="
 name=toric_n6
-run $name --model.n_cells=6 --seed=3425395312
+run $name --model.n_cells=6 --seed=532984323
 
-echo -e "\n\n===== N=8 Metropolis Sampling ====="
-name=toric_n8
-run $name --model.n_cells=8 --seed=342539324213
+# echo -e "\n\n===== N=8 Metropolis Sampling ====="
+# name=toric_n8
+# run $name --model.n_cells=8 --seed=342539324213
--- src/machine/pfaffian.cpp
+++ src/machine/pfaffian.cpp
@@ -286,7 +286,8 @@ pfaff_context pfaffian::get_context(const spin_state& state) const {
 
     Eigen::MatrixXcd mat = get_mat(state);
 
-    ret.inv = mat.inverse();
+    ret.inv = mat.completeOrthogonalDecomposition().pseudoInverse();
+    // ret.inv = mat.inverse()
     ret.inv.noalias() -= ret.inv.transpose().eval();
     ret.inv *= 0.5;
 
